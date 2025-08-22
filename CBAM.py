572fcb2420bdc0f661c03d5ced8a27bf50e033d5
Nitish-0808 <ghanamahala1999@gmail.com>
2025-08-22 18:01:53 +0530

    Chnages Made

--- CBAM.py
+++ CBAM.py
@@ -1,24 +1,23 @@
 import torch
 import torch.nn as nn
-
+## Channel Attention Module of CBAM
 class ChannelAttention(nn.Module):
-  def __init__(self, channel, reduction=16):
-    super().__init__()
-    self.avg_pool=nn.AdaptiveAvgPool2d(1)
-    self.max_pool=nn.AdaptiveMaxPool2d(1)
-    self.fc=nn.Sequential(
-        nn.Linear(channel, channel // reduction, bias=False),
-        nn.ReLU(),
-        nn.Linear(channel // reduction, channel, bias=False)
-    )
-    self.sigmoid=nn.Sigmoid()
-  def forward(self, x):
-    avg_out=self.fc(self.avg_pool(x).squeeze(-1).squeeze(-1))
-    max_out=self.fc(self.max_pool(x).squeeze(-1).squeeze(-1))
-    out=avg_out+max_out
-    return self.sigmoid(out).unsqueeze(-1).unsqueeze(-1)
-  
-
+    def __init__(self, channel, reduction=16):
+        super().__init__()
+        self.avg_pool=nn.AdaptiveAvgPool2d(1)
+        self.max_pool=nn.AdaptiveMaxPool2d(1)
+        self.fc=nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(),
+            nn.Linear(channel // reduction, channel, bias=False)
+            )
+        self.sigmoid=nn.Sigmoid()
+    def forward(self, x):
+        avg_out=self.fc(self.avg_pool(x).squeeze(-1).squeeze(-1))
+        max_out=self.fc(self.max_pool(x).squeeze(-1).squeeze(-1))
+        out=avg_out+max_out
+        return self.sigmoid(out).unsqueeze(-1).unsqueeze(-1)
+##  
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
        super().__init__()
